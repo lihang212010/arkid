70e93e983bc6a22a19d4fe20c71b34e5f31ffe51
longgui-penglei <penglei@longguikeji.com>
2021-08-16 20:17:31 +0800

    feat: 🎸 添加设备管理页面的信息

--- api/v1/pages/device_manage.py
+++ api/v1/pages/device_manage.py
@@ -6,5 +6,24 @@ name = '设备管理'
 
 extend_schema_tags(
     tag,
-    name
+    name,
+    {
+        'type': 'table_page',
+        'init': {
+            'path': '/api/v1/tenant/{tenant_uuid}/device/',
+            'method': 'get'
+        },
+        'global': {
+            'export': {
+                'path': '/api/v1/tenant/{tenant_uuid}/device_export/',
+                'method': 'get'
+            }
+        },
+        'local': {
+            'delete': {
+                'path': '/api/v1/tenant/{tenant_uuid}/device/{device_uuid}/detail/',
+                'method': 'delete'
+            }
+        }
+    }
 )
