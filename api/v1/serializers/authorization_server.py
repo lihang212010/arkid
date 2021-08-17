4087b2096a10572e92243e680920faccf2074c8a
King <guancyxx@guancyxx.cn>
2021-08-17 22:25:36 +0800

    feat: 🎸 链接身份源中英文切换

--- api/v1/serializers/authorization_server.py
+++ api/v1/serializers/authorization_server.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-
+from django.utils.translation import ugettext_lazy as _
 
 class AuthorizationServerSerializer(serializers.Serializer):
 
-    id = serializers.CharField()
-    name = serializers.CharField()
-    description = serializers.CharField()
+    id = serializers.CharField(
+        label=_("唯一标识")
+    )
+    name = serializers.CharField(
+        label=_("名称")
+    )
+    description = serializers.CharField(
+        label=_("描述")
+    )
